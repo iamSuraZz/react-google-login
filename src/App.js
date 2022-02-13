a82a7262597d5fd9d526c2add3c92876c6e66b68
SuraZz007 <surajdev9886@gmail.com>
2022-02-13 23:47:09 +0530

    Insert google client id inside .env file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import "./App.css";
+import GoogleLogin from "react-google-login";
 
 function App() {
+  const handleFailure = (result) => {
+    alert(result);
+  };
+
+  const handleLogin = (googleData) => {
+    console.log(googleData);
+  };
+
   return (
-    <div>
-      <h1 style={{ textAlign: "center" }}>React Google Login !!!</h1>
+    <div className="App">
+      <header className="App-header">
+        <h1>React Google Login !!!</h1>
+        <GoogleLogin
+          clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+          buttonText="Login with Google"
+          onSuccess={handleLogin}
+          onFailure={handleFailure}
+          cookiePolicy={"single_host_origin"}
+        />
+      </header>
     </div>
   );
 }
